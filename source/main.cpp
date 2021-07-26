712a3ef895b82fbb4e520bcc634fbbae6859388e
vikramjitSingh <VIKRAMJIT.singh15@yahoo.com>
2021-07-26 21:28:48 +0530

    CSVReader-ParseData
    Implemented initial version of csv reader and store data in SecuritiesInfo variable

--- source/main.cpp
+++ source/main.cpp
@@ -43,160 +43,133 @@ namespace BTF
 
 						size_t l_columnCount{0};
 						std::string l_token;
-						bool l_bParseHeader {true};
 
-						while ( (!l_bFileMisformed) && (l_pos = l_line.find(l_delimiter)) != std::string::npos ) 
+						while ( !l_line.empty() )
 						{
+							if (l_columnCount < 6)
+							{
+								l_pos = l_line.find(l_delimiter);
+							}
+
+							l_token = l_line.substr(0, l_pos);
+							std::cout << l_token << std::endl;
+
 							if(l_columnCount == 0)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "Date" == l_token)
-								{
-									l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									o_PriceInfo.date.push_back(l_token);
-								}
-								else
+								if((l_uLineCount == 1) && "Date" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: Date"<<std::endl;
 								}
-
-								l_columnCount++;
+								else if (!(l_uLineCount == 1))
+								{
+									o_PriceInfo.date.push_back((l_token));
+								}
 							}
 							else if(l_columnCount == 1)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "Open" == l_token)
-								{
-									l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									o_PriceInfo.open.push_back(std::stof(l_token));
-								}
-								else
+
+								if((l_uLineCount == 1) && "Open" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: Open"<<std::endl;
 								}
+								else if (!(l_uLineCount == 1))
+								{
+									o_PriceInfo.open.push_back(std::stof(l_token));
+								}
 
-								l_columnCount++;
-							} 
+							}
 							else if(l_columnCount == 2)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "High" == l_token)
-								{
-										l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									o_PriceInfo.high.push_back(std::stof(l_token));
-								}
-								else
+								if((l_uLineCount == 1) && "High" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: High"<<std::endl;
 								}
+								else if (!(l_uLineCount == 1))
+								{
+									o_PriceInfo.high.push_back(std::stof(l_token));
+								}
 
-								l_columnCount++;
+
 							}
 							else if(l_columnCount == 3)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "Low" == l_token)
-								{
-									l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									o_PriceInfo.low.push_back(std::stof(l_token));
-								}
-								else
+
+								if((l_uLineCount == 1) && "Low" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: Low"<<std::endl;
 								}
+								else if (!(l_uLineCount == 1))
+								{
+									o_PriceInfo.low.push_back(std::stof(l_token));
+								}
 
-								l_columnCount++;
+
 							}
 							else if(l_columnCount == 4)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "Close" == l_token)
-								{
-									l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									o_PriceInfo.close.push_back(std::stof(l_token));
-								}
-								else
+
+								if((l_uLineCount == 1) && "Close" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: Close"<<std::endl;
 								}
-						
-								l_columnCount++;
+								else if (!(l_uLineCount == 1))
+								{
+									o_PriceInfo.close.push_back(std::stof(l_token));
+								}
+
 							}
 							else if(l_columnCount == 5)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "Adj Close" == l_token)
-								{
-									l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									//o_PriceInfo.adjClose.push_back(std::stof(l_token))
-									continue;
-								}
-								else
+
+								if((l_uLineCount == 1) && "Adj Close" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: Adj Close"<<std::endl;
 								}
+								else if (!(l_uLineCount == 1))
+								{
+									//o_PriceInfo.open.push_back(l_token)
+									/* do nothing */
+								}
 
-								l_columnCount++;
+
 							}
 							else if(l_columnCount == 6)
 							{
-								l_token = l_line.substr(0, l_pos);
-								std::cout << l_token << std::endl;
-								if(l_bParseHeader && "Volume" == l_token)
-								{
-									l_line.erase(0, l_pos + l_delimiter.length());
-								}
-								else if (!l_bParseHeader)
-								{
-									o_PriceInfo.volume.push_back(std::stof(l_token));
-								}
-								else
+
+								if((l_uLineCount == 1) && "Volume" != l_token)
 								{
 									l_bFileMisformed = true;
 									std::cout<<"first line of the file should contain the header: Volume"<<std::endl;
 								}
+								else if (!(l_uLineCount == 1))
+								{
+									o_PriceInfo.volume.push_back(std::stof(l_token));
+								}
 
-								l_columnCount++;
-                            }
+
+								//std::cout<<"value of line after last column: "<<l_line<<std::endl;
+
+							}
 							else
 							{
 								l_columnCount++;
 								l_bFileMisformed = true;
 								std::cout<<"Unexpeced column size: "<< l_columnCount<<std::endl;
+								break;
 							}
+							l_line.erase(0, l_pos + l_delimiter.length());
+							l_columnCount++;
 						}
 
 					}
+
+					std::cout<<"Total lines parsed: "<<l_uLineCount<<std::endl;
 
 					l_file.close();
 				}
@@ -208,40 +181,37 @@ namespace BTF
 
 				return (!l_bFileMisformed);
 			}
-
-
 	};
-
-
-	int main()
-	{
-		SecurityInfo l_PriceInfo;
-		const char* l_sfileName {"Nifty500.csv"};
-		
-		if(CSVReader::ParseData(l_sfileName, l_PriceInfo))
-		{
-			std::cout<<"Stored value ------>>"<<std::endl;
-			/*for(int i= 0; i<l_PriceInfo.open.size(); i++)
-			{
-				std::cout<<l_PriceInfo.date[i]<<", "
-						<<l_PriceInfo.open[i]<<", "
-						<<l_PriceInfo.high[i]<<", "
-						<<l_PriceInfo.low[i]<<", "
-						<<l_PriceInfo.close<<", "
-						<<l_PriceInfo.volume[i]<<std::endl;						
-			}*/
-			for(const auto& val: l_PriceInfo.date)
-			{
-				std::cout<<val<<std::endl;						
-			}
-		}
-		else
-		{
-			std::cout<<"main - failed to parse: "<<l_sfileName<<std::endl;
-		}
-			
-		
-		
-		return 0;
-	}
 }
+
+int main()
+{
+	BTF::SecurityInfo l_PriceInfo;
+	const char* l_sfileName {"Nifty500.csv"};
+
+	std::cout<<l_sfileName<<std::endl;
+	if(BTF::CSVReader::ParseData(l_sfileName, l_PriceInfo))
+	{
+		std::cout<<"Stored value ------>>"<<std::endl;
+		/*for(int i= 0; i<l_PriceInfo.open.size(); i++)
+		{
+			std::cout<<l_PriceInfo.date[i]<<", "
+					<<l_PriceInfo.open[i]<<", "
+					<<l_PriceInfo.high[i]<<", "
+					<<l_PriceInfo.low[i]<<", "
+					<<l_PriceInfo.close<<", "
+					<<l_PriceInfo.volume[i]<<std::endl;
+		}*/
+		for(const auto& val: l_PriceInfo.date)
+		{
+			std::cout<<val<<std::endl;
+		}
+	}
+	else
+	{
+		std::cout<<"main - failed to parse: "<<l_sfileName<<std::endl;
+	}
+
+	return 0;
+}
+
